f2d2789bd09cf65f08c1dd5f1244563e609b6547
lperpinya1974 <lperpinya1974@gmail.com>
2017-02-15 09:56:46 +0100

    LifeCycle de Game

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,33 +1,49 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MyGdxGame extends ApplicationAdapter {
+public class MyGdxGame extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		Gdx.app.log("LifeCyCle", "create");
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
+		//Gdx.app.log("LifeCyCle", "render");
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		Gdx.app.log("LifeCyCle", "dispose");
+	}
+
+	@Override
+	public void pause() {
+		super.pause();
+		Gdx.app.log("LifeCyCle", "pause");
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		super.resize(width, height);
+		Gdx.app.log ("LifeCytcle", "resize(" + Integer.toString(width) + "," + Integer.toString(height)+")");
+	}
+
+	@Override
+	public void resume() {
+		super.resume();
+		Gdx.app.log("LifeCyCle", "resume");
 	}
 }
